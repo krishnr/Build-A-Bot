ecb6ede1307877cad620eac909a7b5f80547bcd8
Krishn Ramesh <krishnr@gmail.com>
2016-07-11 14:32:21 -0400

    add image upload functionality

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('.product-sales').hide();
-  $('.lead-gen').show();
+  $('.lead-gen').hide();
   $('.objective-dropdown').change(function () {
     $('.lead-gen').fadeOut('fast');
     $('.product-sales').fadeOut('fast');
@@ -13,17 +13,45 @@ $('.business-name-input').keyup(function() {
 });
 
 $(document)
-		.on('focus.textarea', '.autoExpand', function(){
-			var savedValue = this.value;
-			this.value = '';
-			this.baseScrollHeight = this.scrollHeight;
-			this.value = savedValue;
-		})
-		.on('input.textarea', '.autoExpand', function(){
-			var minRows = this.getAttribute('data-min-rows')|0,
-				 rows;
-			this.rows = minRows;
-        console.log(this.scrollHeight , this.baseScrollHeight);
-			rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
-			this.rows = minRows + rows;
-		});
+	.on('focus.textarea', '.autoExpand', function(){
+		var savedValue = this.value;
+		this.value = '';
+		this.baseScrollHeight = this.scrollHeight;
+		this.value = savedValue;
+	})
+	.on('input.textarea', '.autoExpand', function(){
+		var minRows = this.getAttribute('data-min-rows')|0,
+			 rows;
+		this.rows = minRows;
+    console.log(this.scrollHeight , this.baseScrollHeight);
+		rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
+		this.rows = minRows + rows;
+	});
+
+$(document).ready(function() {
+  $.uploadPreview({
+    input_field: "#image-upload-1",
+    preview_box: "#image-preview-1",
+    label_field: "#image-label-1"
+  });
+  $.uploadPreview({
+    input_field: "#image-upload-2",
+    preview_box: "#image-preview-2",
+    label_field: "#image-label-2"
+  });
+  $.uploadPreview({
+    input_field: "#image-upload-3",
+    preview_box: "#image-preview-3",
+    label_field: "#image-label-3"
+  });
+  $.uploadPreview({
+    input_field: "#image-upload-3",
+    preview_box: "#image-preview-3",
+    label_field: "#image-label-3"
+  });
+  $.uploadPreview({
+    input_field: "#image-upload-4",
+    preview_box: "#image-preview-4",
+    label_field: "#image-label-4"
+  });
+});
